63a450b3b978b5f02800c1b91f66987dcd56f92f
VersuchDrei <falkomitc@gmail.com>
2021-01-05 16:36:15 +0100

    Added an event for later use.

---
+++ src/com/versuchdrei/menumanager/implementations/events/InventoryOverflowEvent.java
@@ -0,0 +1,51 @@
+package com.versuchdrei.menumanager.implementations.events;
+
+import java.util.List;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class InventoryOverflowEvent extends PlayerEvent implements Cancellable{
+	
+	private static final HandlerList handlers = new HandlerList();
+
+	private List<ItemStack> items;
+	private boolean cancelled = false;
+	
+	InventoryOverflowEvent(final Player player, final List<ItemStack> items) {
+		super(player);
+		
+		this.items = items;
+	}
+	
+	public List<ItemStack> getItems(){
+		return this.items;
+	}
+	
+	public void setItems(final List<ItemStack> items) {
+		this.items = items;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return this.cancelled;
+	}
+
+	@Override
+	public void setCancelled(final boolean cancelled) {
+		this.cancelled = cancelled;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return InventoryOverflowEvent.handlers;
+	}
+	
+	public static HandlerList getHandlerList() {
+		return InventoryOverflowEvent.handlers;
+	}
+
+}
